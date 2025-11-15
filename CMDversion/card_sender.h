aaf85633360aa72395c708cd6b4504a858723d72
Altre <12987401+altre@user.noreply.gitee.com>
2025-11-15 20:42:11 +0800

    测试可运行

--- CMDversion/card_sender.h
+++ CMDversion/card_sender.h
@@ -4,15 +4,6 @@
 #include "config/interface.h"
 #include "config/card.h"
 
-/*
-typedef struct CardSender {
-    IMessageReceiver msg_recive_interface; // 实现消息接收接口
-} CardSender;
-
-CardSender new_cardsender(void);
-*/
-
-
 Card* cardsenderProcesser(int num_cards);
 
 #endif // CARD_SENDER_H
--- CMDversion/dice_roller.c
+++ CMDversion/dice_roller.c
@@ -1,7 +1,11 @@
 #include "dice_roller.h"
+#include <time.h>
+#include <stdlib.h>
 
 int rollDice(int numberOfDice, int sidesPerDie) {
     int total = 0;
+    // 使用当前时间作为随机数种子
+    srand((unsigned int)time(NULL));
     for (int i = 0; i < numberOfDice; i++) {
         total += (rand() % sidesPerDie) + 1; // 掷骰子，结果在1到sidesPerDie之间
     }
@@ -9,6 +13,8 @@ int rollDice(int numberOfDice, int sidesPerDie) {
 }
 
 int generateRandomNumber(int min, int max){
+    // 使用当前时间作为随机数种子
+    srand((unsigned int)time(NULL));
     int res = rand() % (max - min + 1) + min;
     return res;
 }
